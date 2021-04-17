cd7fd5bf50045fd17f7d8e51c8e23650dcf4feb2
Rohit Lode <rohitlode@Rohits-MBP.lan1>
2021-04-16 20:38:34 -0500

    Added users API

--- routes/api/users.js
+++ routes/api/users.js
@@ -19,17 +19,18 @@ router.get("/", async (req, res) => {
 })
 
 //Getting one
-router.get("/:id", (req, res) => {
-    console.log(req.params.id);
+router.get("/:id", getUser, (req, res) => {
+    res.send(res.user);
 })
 
 
 //Creating one
-router.post("/", async (req, res) => {
+router.post("/" , async (req, res) => {
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        address: req.body.address,
+        registeredDate: req.body.registeredDate
     })
     console.log(req);
     try{
@@ -38,7 +39,7 @@ router.post("/", async (req, res) => {
                  console.log(err);
                  return;
             }
-            console.log("Created Successfully");
+            console.log("user Created Successfully");
             res.status(201).json(newUser)
       });
     }catch(err){
@@ -48,13 +49,65 @@ router.post("/", async (req, res) => {
 
 
 //Updating one
-router.patch("/", (req, res) => {
+router.patch("/:id", getUser, async(req, res) => {
+    if(req.body.name != null){
+        res.user.name = req.body.name
+    }
+    if(req.body.address != null){
+        res.user.address = req.body.address
+    }
+    if(req.body.mobile != null){
+        res.user.mobile = req.body.mobile
+    }
+    if(req.body.email != null){
+        res.user.email = req.body.email
+    }
+    if(req.body.registeredDate != null){
+        res.user.registeredDate = req.body.registeredDate
+    }
+    if(req.body.password != null){
+        res.user.password = req.body.password
+    }
+    if(req.body.dob != null){
+        res.user.password = req.body.dob
+    }
 
+    try{
+        const updatedUser=  await res.user.save()
+        console.log("Updated user Successfully");
+        res.json(updatedUser)
+    }catch(err){
+        res.status(400).json({ message: err.message })
+    }
 })
 
 //Deleting one
-router.delete("/", (req, res) => {
-
+router.delete("/:id", getUser, async (req, res) => {
+    try{
+        await res.user.remove()
+        res.json({message: 'Deleted user'})
+    }catch(err){
+        return res.status(500).json({message: err.message})
+    }
 })
+
+
+
+async function getUser(req, res, next){
+    let user;
+    try{
+        user = await User.findById(req.params.id)
+        if(user == null){
+            return res.status(404).json({message: 'Cannot Find user'})
+        }
+    }catch(err){
+        return res.status(500).json({message: err.message})
+    }
+
+    res.user = user;
+    next()
+}
+
+
 
 module.exports = router
